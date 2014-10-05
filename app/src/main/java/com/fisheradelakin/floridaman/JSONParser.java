272ea7f57e419fbba6d039eb00d731098506d79d
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-10-05 08:29:51 -0700

    change the background

--- app/src/main/java/com/fisheradelakin/floridaman/JSONParser.java
+++ app/src/main/java/com/fisheradelakin/floridaman/JSONParser.java
@@ -6,7 +6,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
